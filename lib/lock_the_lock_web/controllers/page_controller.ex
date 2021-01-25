60e8211746da4711dd307131e81633a9c0a45d40
Sebastian Borrazas <seba.borrazas@gmail.com>
2021-01-24 21:32:49 -0300

    Handle GET /:lock_id route

--- lib/lock_the_lock_web/controllers/page_controller.ex
+++ lib/lock_the_lock_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule LockTheLockWeb.PageController do
   use LockTheLockWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_resp_header("content-type", "text/html; charset=utf-8")
+    |> Plug.Conn.send_file(200, Application.app_dir(:lock_the_lock, "priv/static/index.html"))
   end
 end
